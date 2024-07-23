be099b5ecd61eb19fc1281926d471caae699b26a
KimWang906 <hyunbin06git@gmail.com>
2024-07-23 23:11:27 +0900

    기능 추가: 카운트 다운, 유저 랜덤 텔레포트, 버그 픽스: 설정된 내용이 파일에 적용되지 않던 문제 해결

--- src/main/java/org/kw906plugin/catchTail/events/EntityDamage.java
+++ src/main/java/org/kw906plugin/catchTail/events/EntityDamage.java
@@ -1,4 +1,38 @@
 package org.kw906plugin.catchTail.events;
 
-public class EntityDamage {
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.kw906plugin.catchTail.GameStatus;
+import org.kw906plugin.catchTail.SendMessage;
+import org.kw906plugin.catchTail.commands.Sequence;
+
+public class EntityDamage implements Listener {
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            if (GameStatus.getStatus() == GameStatus.COUNT_DOWN) {
+                event.setCancelled(true);
+                SendMessage.sendMessagePlayer(player, Component.text("게임 시작전에는 공격하실 수 없습니다")
+                                                               .color(NamedTextColor.RED));
+                return;
+            }
+
+            if (GameStatus.getStatus() != GameStatus.RUNNING)
+                return;
+
+            if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK)
+                return;
+
+            if (event.getDamage() >= player.getHealth()) {
+                event.setDamage(0);
+                Sequence.out(player);
+            }
+        }
+    }
+
+
 }
