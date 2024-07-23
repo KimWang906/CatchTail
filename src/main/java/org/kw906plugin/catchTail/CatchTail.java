93462111609b9ccea38c6f82fe885e028f3a1fe7
KimWang906 <hyunbin06git@gmail.com>
2024-07-23 20:58:11 +0900

    기능 추가: 카운트 다운, 유저 랜덤 텔레포트, 버그 픽스: 설정된 내용이 파일에 적용되지 않던 문제 해결

--- src/main/java/org/kw906plugin/catchTail/CatchTail.java
+++ src/main/java/org/kw906plugin/catchTail/CatchTail.java
@@ -1,6 +1,5 @@
 package org.kw906plugin.catchTail;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.kw906plugin.catchTail.commands.Configure;
 import org.kw906plugin.catchTail.commands.GameController;
@@ -15,7 +14,6 @@ public final class CatchTail extends JavaPlugin {
         // Plugin startup logic
         config = new Configure();
         getCommand("catch-tail").setExecutor(new GameController());
-
         getLogger().info("CatchTail 플러그인이 활성화 되었습니다.");
     }
 
@@ -23,5 +21,6 @@ public final class CatchTail extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("CatchTail 플러그인이 비활성화 되었습니다.");
+        saveConfig();
     }
 }
