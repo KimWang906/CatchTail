d8b101af82d47b03c1ccfa900b51565ae4e5b8aa
Girey0211 <kkswin1234@gmail.com>
2024-07-25 10:46:52 +0900

    debug: player stun

--- src/main/java/org/kw906plugin/catchTail/events/PlayerMovement.java
+++ src/main/java/org/kw906plugin/catchTail/events/PlayerMovement.java
@@ -1,9 +1,11 @@
 package org.kw906plugin.catchTail.events;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.kw906plugin.catchTail.SendMessage;
 import org.kw906plugin.catchTail.commands.Sequence;
 
 public class PlayerMovement implements Listener {
@@ -11,6 +13,7 @@ public class PlayerMovement implements Listener {
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         if (Sequence.checkPlayerStun(player)) {
+            SendMessage.sendActionBar(player, Component.text("기절!"));
             event.setCancelled(true);
             player.teleport(event.getFrom());
         } else {
